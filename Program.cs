9c61501339e2263bbaaad24185ad8793a676c9cc
Fear Me <samprivate@live.com>
2014-09-20 19:40:35 +0200

    oeps

--- Program.cs
+++ Program.cs
@@ -6,18 +6,26 @@ namespace BitmapVisualizer
 {
     class Program
     {
-        static readonly string stuff = @"C:\Users\Sam\Desktop\gg.bmp";
-
         static void Main(string[] args)
         {
-            Bitmap image = new Bitmap(stuff, true);
-            Dictionary<Point, Color> layout = new Dictionary<Point, Color>();
+            if (args.Length > 0)
+            {
+                try
+                {
+                    Bitmap image = new Bitmap(args[0], true);
+                    Dictionary<Point, Color> layout = new Dictionary<Point, Color>();
 
-            for (int y = 0; y < image.Height; y++)
-                for (int x = 0; x < image.Width; x++)
-                    layout.Add(new Point(x, y), image.GetPixel(x, y));
-
-            Console.Write(new Layout(layout).ToString());
+                    for (int y = 0; y < image.Height; y++)
+                        for (int x = 0; x < image.Width; x++)
+                            layout.Add(new Point(x, y), image.GetPixel(x, y));
+                    Console.Write(new Layout(layout).ToString());
+                }
+                catch
+                {
+                    Console.WriteLine(args[0] + " not found");
+                }
+            } else
+                Console.WriteLine("No args");
             Console.Read();
         }
     }
