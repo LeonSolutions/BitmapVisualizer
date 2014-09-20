65f18b9ddfe846aed913951a9ee2b4cf869fc0a1
Fear Me <samprivate@live.com>
2014-09-20 20:54:36 +0200

    ayy lmao es fix d'general lolkbye

--- Layout.cs
+++ Layout.cs
@@ -1,20 +1,17 @@
 ﻿using System.Collections.Generic;
 using System.Drawing;
 
-namespace BitmapVisualizer
-{
-    class Layout
-    {
+namespace BitmapVisualizer {
+    class Layout {
+
         private readonly Dictionary<Point, Color> layout;
         private readonly int width;
 
-        public Layout(Dictionary<Point, Color> layout)
-        {
+        public Layout(Dictionary<Point, Color> layout) {
             this.layout = layout;
 
             int biggestX = 0;
-            foreach (Point p in layout.Keys)
-            {
+            foreach (Point p in layout.Keys) {
                 if (p.Y > 0)
                     break;
                 if (p.X > biggestX)
@@ -23,21 +20,22 @@ namespace BitmapVisualizer
             width = biggestX;
         }
 
-        public override string ToString()
-        {
+        public override string ToString() {
+            return ToString(Color.Black);
+        }
+
+        public string ToString(Color draw) {
             string str = "";
-            foreach (Point p in layout.Keys)
-            {
-                str += (IsBlack(layout[p]) ? "x" : " ");
+            foreach (Point p in layout.Keys) {
+                str += (IsSameColor(layout[p], draw) ? "x" : " ");
                 if (p.X == width)
                     str += "\r\n";
             }
             return str;
         }
 
-        public static bool IsBlack(Color c)
-        {
-            return (c.R + c.G + c.B) == 0;
+        public static bool IsSameColor(Color c1, Color c2) {
+            return c1.R == c2.R && c1.G == c2.G && c1.B == c2.B;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,31 +2,27 @@
 using System.Collections.Generic;
 using System.Drawing;
 
-namespace BitmapVisualizer
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            if (args.Length > 0)
-            {
-                try
-                {
+namespace BitmapVisualizer {
+    class Program {
+
+        static void Main(string[] args) {
+            if (args.Length > 0) {
+                try {
                     Bitmap image = new Bitmap(args[0], true);
                     Dictionary<Point, Color> layout = new Dictionary<Point, Color>();
 
                     for (int y = 0; y < image.Height; y++)
                         for (int x = 0; x < image.Width; x++)
                             layout.Add(new Point(x, y), image.GetPixel(x, y));
+                    image.Dispose();
                     Console.Write(new Layout(layout).ToString());
-                }
-                catch
-                {
-                    Console.WriteLine(args[0] + " not found");
+                } catch {
+                    Console.WriteLine("No file called " + args[0] + " found");
                 }
             } else
                 Console.WriteLine("No args");
-            Console.Read();
+            Console.Write("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
