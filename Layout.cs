2bcf0fe020cab2ed98b51fa6e2c838f36aede34f
Fear Me <samprivate@live.com>
2014-09-20 19:20:42 +0200

    Release

---
+++ Layout.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace BitmapVisualizer
+{
+    class Layout
+    {
+        private readonly Dictionary<Point, Color> layout;
+        private readonly int width;
+
+        public Layout(Dictionary<Point, Color> layout)
+        {
+            this.layout = layout;
+
+            int biggestX = 0;
+            foreach (Point p in layout.Keys)
+            {
+                if (p.Y > 0)
+                    break;
+                if (p.X > biggestX)
+                    biggestX = p.X;
+            }
+            width = biggestX;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach (Point p in layout.Keys)
+            {
+                str += (IsBlack(layout[p]) ? "x" : " ");
+                if (p.X == width)
+                    str += "\r\n";
+            }
+            return str;
+        }
+
+        public static bool IsBlack(Color c)
+        {
+            return (c.R + c.G + c.B) == 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace BitmapVisualizer
+{
+    class Program
+    {
+        static readonly string stuff = @"C:\Users\Sam\Desktop\gg.bmp";
+
+        static void Main(string[] args)
+        {
+            Bitmap image = new Bitmap(stuff, true);
+            Dictionary<Point, Color> layout = new Dictionary<Point, Color>();
+
+            for (int y = 0; y < image.Height; y++)
+                for (int x = 0; x < image.Width; x++)
+                    layout.Add(new Point(x, y), image.GetPixel(x, y));
+
+            Console.Write(new Layout(layout).ToString());
+            Console.Read();
+        }
+    }
+}
